993facbbb3156070a499ac4dc6d211df89ebb301
Thomas Kaczmarek <mulenatic@gmail.com>
2016-07-18 21:13:07 +0200

    Added new component for the tabulature.

---
+++ client/app/components/TabComponent/TabComponent.ts
@@ -0,0 +1,7 @@
+import {Component} from "@angular/core";
+
+@Component({
+    selector: "tabComponent",
+    templateUrl: "../../../app/components/TabComponent/TabComponent.html"
+})
+export class TabComponent { };
--- client/app/main.ts
+++ client/app/main.ts
@@ -1,11 +1,12 @@
 import {Component} from "@angular/core";
 import {bootstrap} from "@angular/platform-browser-dynamic";
 import {NavbarComponent} from "./components/navbar/navbar";
+import {TabComponent} from "./components/TabComponent/TabComponent";
 
 @Component({
     selector: "app",
     templateUrl: "app/main.html",
-    directives: [NavbarComponent]
+    directives: [NavbarComponent, TabComponent]
 })
 export class AppComponent { };
 
