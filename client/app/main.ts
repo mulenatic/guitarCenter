aab5d7fef1ae17978e25b6b88b8fe0a2a49f6b4e
Thomas Kaczmarek <mulenatic@gmail.com>
2016-07-18 21:17:35 +0200

    Added a dummy component for the Metronome and Timer.

--- client/app/main.ts
+++ client/app/main.ts
@@ -2,11 +2,12 @@ import {Component} from "@angular/core";
 import {bootstrap} from "@angular/platform-browser-dynamic";
 import {NavbarComponent} from "./components/navbar/navbar";
 import {TabComponent} from "./components/TabComponent/TabComponent";
+import {MetroTimerComponent} from "./components/MetroTimerComponent/MetroTimerComponent";
 
 @Component({
     selector: "app",
     templateUrl: "app/main.html",
-    directives: [NavbarComponent, TabComponent]
+    directives: [NavbarComponent, TabComponent, MetroTimerComponent]
 })
 export class AppComponent { };
 
