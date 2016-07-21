9b771370db9005f458480d66a3ea7e53444c3d8e
Thomas Kaczmarek <mulenatic@gmail.com>
2016-07-21 22:51:38 +0200

    The standard notation can now be toggled via a button click.

--- client/app/components/TabComponent/TabComponent.ts
+++ client/app/components/TabComponent/TabComponent.ts
@@ -3,39 +3,57 @@ import {VexTab, Artist, Flow} from "node_modules/vextab/releases/vextab-div.js";
 
 @Component({
     selector: "tabComponent",
-    templateUrl: "../../../app/components/TabComponent/TabComponent.html",
-    styles: ["canvas { border: 2px solid black; }"]
+    templateUrl: "../../../app/components/TabComponent/TabComponent.html"
 })
 export class TabComponent implements OnInit {
 
     renderer: any;
-    artist: any;
-    vextab: any;
+
+    showStandardNotation: boolean = false;
 
     constructor(private elementRef: ElementRef) { }
 
     ngOnInit() {
 
         let Renderer = Flow.Renderer;
-
-        let tabWidth = this.elementRef.nativeElement.parentElement.offsetWidth;
-
-
         this.renderer = new Renderer("tab", Renderer.Backends.CANVAS);
 
-        this.artist = new Artist(10, 10, tabWidth - 10, { scale: 1.0 });
-        this.vextab = new VexTab(this.artist);
+        this.renderTab();
+
+    }
+
+    toggleShowStandardNotation(): void {
+
+        this.showStandardNotation = !this.showStandardNotation;
+        this.renderTab();
+
+    }
+
+    onOrOff(): string {
+
+        return this.showStandardNotation ? "on" : "off";
+
+    }
+
+    renderTab(): void {
 
         try {
 
-            let text1: String = "tabstave notation=true\n notes =|: :16 1-2-3-4/6  1-2-3-4/5  1-2-3-4/4  1-2-3-4/3 | 1-2-3-4/2 1-2-3-4/1 1-2-3-4/2 1-2-3-4/3 ";
+            let tabWidth = this.elementRef.nativeElement.parentElement.offsetWidth;
 
-            this.vextab.parse(text1);
+            let artist = new Artist(10, 10, tabWidth - 10, { scale: 1.0 });
+            let vextab = new VexTab(artist);
 
-            let text2: String = "tabstave notation=true\n notes | :16 1-2-3-4/4 1-2-3-4/5 1-2-3-4/6 =:|";
+            let text1: String = `tabstave notation=${this.showStandardNotation}
+notes =|: :16 1-2-3-4/6  1-2-3-4/5  1-2-3-4/4  1-2-3-4/3 | 1-2-3-4/2 1-2-3-4/1 1-2-3-4/2 1-2-3-4/3 `;
 
-            this.vextab.parse(text2);
-            this.artist.render(this.renderer);
+            vextab.parse(text1);
+
+            let text2: String = `tabstave notation=${this.showStandardNotation}
+notes | :16 1-2-3-4/4 1-2-3-4/5 1-2-3-4/6 =:|`;
+
+            vextab.parse(text2);
+            artist.render(this.renderer);
 
 
         } catch (e) {
