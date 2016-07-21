48a7d486e5c926b275beb87a459b5eae36fad8f8
Thomas Kaczmarek <mulenatic@gmail.com>
2016-07-21 22:17:29 +0200

    The tab will now adjust to Component size.
    The tab now adjust to the size of the component. This is achieved by
    getting the width of the element, that contains the componnent.

--- client/app/components/TabComponent/TabComponent.ts
+++ client/app/components/TabComponent/TabComponent.ts
@@ -1,9 +1,10 @@
-import {Component, OnInit} from "@angular/core";
+import {Component, OnInit, ElementRef} from "@angular/core";
 import {VexTab, Artist, Flow} from "node_modules/vextab/releases/vextab-div.js";
 
 @Component({
     selector: "tabComponent",
-    templateUrl: "../../../app/components/TabComponent/TabComponent.html"
+    templateUrl: "../../../app/components/TabComponent/TabComponent.html",
+    styles: ["canvas { border: 2px solid black; }"]
 })
 export class TabComponent implements OnInit {
 
@@ -11,22 +12,32 @@ export class TabComponent implements OnInit {
     artist: any;
     vextab: any;
 
+    constructor(private elementRef: ElementRef) { }
+
     ngOnInit() {
 
         let Renderer = Flow.Renderer;
 
+        let tabWidth = this.elementRef.nativeElement.parentElement.offsetWidth;
+
+
         this.renderer = new Renderer("tab", Renderer.Backends.CANVAS);
 
-        this.artist = new Artist(10, 10, 600, { scale: 0.8 });
+        this.artist = new Artist(10, 10, tabWidth - 10, { scale: 1.0 });
         this.vextab = new VexTab(this.artist);
 
         try {
 
-            let text: String = "tabstave notation=true\n notes =|: :16 1-2-3-4/6  1-2-3-4/5  1-2-3-4/4  1-2-3-4/3 | 1-2-3-4/2 1-2-3-4/1 1-2-3-4/2 1-2-3-4/3 | 1-2-3-4/4 1-2-3-4/5 1-2-3-4/6 =:|";
+            let text1: String = "tabstave notation=true\n notes =|: :16 1-2-3-4/6  1-2-3-4/5  1-2-3-4/4  1-2-3-4/3 | 1-2-3-4/2 1-2-3-4/1 1-2-3-4/2 1-2-3-4/3 ";
 
-            this.vextab.parse(text);
+            this.vextab.parse(text1);
 
+            let text2: String = "tabstave notation=true\n notes | :16 1-2-3-4/4 1-2-3-4/5 1-2-3-4/6 =:|";
+
+            this.vextab.parse(text2);
             this.artist.render(this.renderer);
+
+
         } catch (e) {
             console.log(e);
         }
