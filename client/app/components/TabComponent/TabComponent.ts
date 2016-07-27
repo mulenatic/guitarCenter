3a9398513235ad5424738611e10be8e2eed6000a
Thomas Kaczmarek <mulenatic@gmail.com>
2016-07-27 12:43:50 +0200

    Moved the vexTab part into separate component.
    All vexTAb stuff is put into a separate component, which will only print
    the tabs. The tabs themselves and various render options will be
    provided by the parent component (not yet completed).

--- client/app/components/TabComponent/TabComponent.ts
+++ client/app/components/TabComponent/TabComponent.ts
@@ -1,76 +1,26 @@
-import {Component, OnInit, ElementRef} from "@angular/core";
+import {Component} from "@angular/core";
 
-import {TabService} from "../../services/TabService/TabService"
-
-import {VexTab, Artist, Flow} from "node_modules/vextab/releases/vextab-div.js";
+import {VexTabComponent} from "../../components/VexTabComponent/VexTabComponent";
 
 @Component({
     selector: "tabComponent",
     templateUrl: "../../../app/components/TabComponent/TabComponent.html",
-    providers: [TabService]
-})
-export class TabComponent implements OnInit {
+    directives: [VexTabComponent]
 
-    renderer: any;
+})
+export class TabComponent {
 
     showStandardNotation: boolean = false;
-
-    constructor(private elementRef: ElementRef, private tabService: TabService) { }
-
-    ngOnInit() {
-
-        let Renderer = Flow.Renderer;
-        this.renderer = new Renderer("tab", Renderer.Backends.CANVAS);
-
-        this.renderTab();
-
-    }
 
     toggleShowStandardNotation(): void {
 
         this.showStandardNotation = !this.showStandardNotation;
-        this.renderTab();
 
     }
 
     onOrOff(): string {
 
         return this.showStandardNotation ? "off" : "on";
-
-    }
-
-    renderTab(): void {
-
-        try {
-
-            let tabWidth = this.elementRef.nativeElement.parentElement.offsetWidth;
-
-            let artist = new Artist(10, 10, tabWidth - 10, { scale: 1.0 });
-            let vextab = new VexTab(artist);
-
-            let tab: Array<string> = this.tabService.getTab();
-
-            tab.forEach(line => {
-
-                let text1: string = this.getTabHeaderString(this.showStandardNotation).concat(line);
-
-                vextab.parse(text1);
-            });
-
-
-            artist.render(this.renderer);
-
-
-        } catch (e) {
-            console.log(e);
-        }
-
-    }
-
-    getTabHeaderString(isStandardnotationVisible: boolean): string {
-
-        return `tabstave notation=${isStandardnotationVisible}
-`;
 
     }
 
---
+++ client/app/components/VexTabComponent/VexTabComponent.ts
@@ -0,0 +1,71 @@
+import {Component, OnInit, ElementRef, Input} from "@angular/core";
+
+import {TabService} from "../../services/TabService/TabService"
+
+import {VexTab, Artist, Flow} from "node_modules/vextab/releases/vextab-div.js";
+
+@Component({
+    selector: "vexTabComponent",
+    templateUrl: "../../../app/components/VexTabComponent/VexTabComponent.html",
+    providers: [TabService]
+})
+export class VexTabComponent implements OnInit {
+
+    renderer: any;
+
+    private _showStandardNotation: boolean;
+
+    @Input()
+    set showStandardNotation(value: boolean) {
+        this._showStandardNotation = value != undefined ? value : false;
+        this.renderTab();
+    };
+
+    constructor(private elementRef: ElementRef, private tabService: TabService) { }
+
+    ngOnInit() {
+
+        let Renderer = Flow.Renderer;
+        this.renderer = new Renderer("tab", Renderer.Backends.CANVAS);
+
+        this.renderTab();
+
+    }
+
+    renderTab(): void {
+
+        try {
+
+            let tabWidth = this.elementRef.nativeElement.parentElement.offsetWidth;
+            tabWidth = 600;
+
+            let artist = new Artist(10, 10, tabWidth - 10, { scale: 1.0 });
+            let vextab = new VexTab(artist);
+
+            let tab: Array<string> = this.tabService.getTab();
+
+            tab.forEach(line => {
+
+                let text1: string = this.getTabHeaderString(this._showStandardNotation).concat(line);
+
+                vextab.parse(text1);
+            });
+
+
+            artist.render(this.renderer);
+
+
+        } catch (e) {
+            console.log(e);
+        }
+
+    }
+
+    getTabHeaderString(isStandardnotationVisible: boolean): string {
+
+        return `tabstave notation=${isStandardnotationVisible}
+`;
+
+    }
+
+};
