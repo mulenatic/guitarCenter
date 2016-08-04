46545bef147d0bb5911cf73eeaef2720ea413a9a
Thomas Kaczmarek <mulenatic@gmail.com>
2016-08-04 21:33:43 +0200

    Added a static version of the form.

--- client/app/components/BookFormComponent/BookFormComponent.ts
+++ client/app/components/BookFormComponent/BookFormComponent.ts
@@ -1,9 +1,20 @@
 import {Component} from "@angular/core";
 
+import {Book} from "../../domain/Book/Book";
+import {BookBuilder} from "../../domain/Book/BookBuilder";
+
 @Component({
     selector: "bookForm",
     templateUrl: "app/components/BookFormComponent/BookFormComponent.html"
 })
 export class BookFormComponent {
+
+    book: Book;
+    bookBuilder: BookBuilder;
+
+    constructor() {
+        this.bookBuilder = new BookBuilder();
+    }
+
 
 }
--- client/app/domain/Book/BookBuilder.ts
+++ client/app/domain/Book/BookBuilder.ts
@@ -2,7 +2,7 @@ import {Book} from "./Book";
 
 export class BookBuilder {
 
-    constructor(public title: string, public author: string, public description: string) { }
+    constructor(public title?: string, public author?: string, public description?: string) { }
 
     build(): Book {
         return new Book(this.title, this.author, this.description);
