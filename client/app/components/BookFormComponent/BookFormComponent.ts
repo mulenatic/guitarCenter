b0fe2a166b299f8e53685c654e996b8c64a54ebb
Thomas Kaczmarek <mulenatic@gmail.com>
2016-08-04 21:13:35 +0200

    Added a component for creating books.
    Added the component and replaced it with the TabComponent in main.ts.

---
+++ client/app/components/BookFormComponent/BookFormComponent.ts
@@ -0,0 +1,9 @@
+import {Component} from "@angular/core";
+
+@Component({
+    selector: "bookForm",
+    templateUrl: "app/components/BookFormComponent/BookFormComponent.html"
+})
+export class BookFormComponent {
+
+}
--- client/app/main.ts
+++ client/app/main.ts
@@ -3,13 +3,14 @@ import {bootstrap} from "@angular/platform-browser-dynamic";
 import {NavbarComponent} from "./components/navbar/navbar";
 import {TabComponent} from "./components/TabComponent/TabComponent";
 import {MetroTimerComponent} from "./components/MetroTimerComponent/MetroTimerComponent";
+import {BookFormComponent} from "./components/BookFormComponent/BookFormComponent";
 
 import {TabService} "./services/TabService/TabService";
 
 @Component({
     selector: "app",
     templateUrl: "app/main.html",
-    directives: [NavbarComponent, TabComponent, MetroTimerComponent]
+    directives: [NavbarComponent, TabComponent, MetroTimerComponent, BookFormComponent]
 })
 export class AppComponent { };
 
