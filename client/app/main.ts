454abd6ddef1ee79f8667ba35295bf508eff0045
Thomas Kaczmarek <mulenatic@gmail.com>
2016-08-07 13:56:05 +0200

    Included router package and basic setup.

--- client/app/main.ts
+++ client/app/main.ts
@@ -1,6 +1,8 @@
 import {Component} from "@angular/core";
 import {disableDeprecatedForms, provideForms} from "@angular/forms";
 import {bootstrap} from "@angular/platform-browser-dynamic";
+import { ROUTER_DIRECTIVES } from "@angular/router";
+
 import {NavbarComponent} from "./components/navbar/navbar";
 import {TabComponent} from "./components/TabComponent/TabComponent";
 import {MetroTimerComponent} from "./components/MetroTimerComponent/MetroTimerComponent";
