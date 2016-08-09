6f86b090e0aa80fb0594fa0a62b76b1faa9e6884
Thomas Kaczmarek <mulenatic@gmail.com>
2016-08-09 15:38:38 +0200

    Reworked the exercise class and added a Builder.

--- client/app/domain/Exercise/Exercise.ts
+++ client/app/domain/Exercise/Exercise.ts
@@ -1,32 +1,22 @@
 export class Exercise {
 
-    private _lines: Array<string>;
+    constructor(private _id: number, private _title: string, private _description: string, private _lines: Array<string>) { }
+
+    get id() {
+        return this._id;
+    }
+
     get lines() {
         return this._lines;
     }
 
-    private _book: string;
-    get book() {
-        return this._book;
-    }
-
-    private _title: string;
     get title() {
         return this._title;
     }
 
-    private _description: string;
     get description() {
         return this._description;
     }
 
-    constructor(book: string, lines: Array<string>, title: string, description: string) {
-
-        this._lines = lines;
-        this._book = book;
-        this._title = title;
-        this._description = description;
-
-    }
 
 }
---
+++ client/app/domain/Exercise/ExerciseBuilder.ts
@@ -0,0 +1,15 @@
+import {Exercise} from "./Exercise";
+
+export class ExerciseBuilder {
+
+    constructor(public id: number, public title: string, public description: string, public lines = new Array<string>()) { }
+
+    build(): Exercise {
+        return new Exercise(this.id, this.title, this.description, this.lines);
+    }
+
+    fromExercise(exercise: Exercise): ExerciseBuilder {
+        return new ExerciseBuilder(exercise.id, exercise.title, exercise.description, exercise.lines);
+    }
+
+}
