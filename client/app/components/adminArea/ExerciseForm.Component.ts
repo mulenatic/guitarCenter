4b75d465affc148065f8f530134a90df1743b5f9
Thomas Kaczmarek <mulenatic@gmail.com>
2016-08-09 15:52:17 +0200

    The new exercise Form is now shown.

--- client/app/components/adminArea/ExerciseForm.Component.ts
+++ client/app/components/adminArea/ExerciseForm.Component.ts
@@ -1,9 +1,28 @@
-import {Component} from "@angular/core";
+import {Component, Input, OnChanges, SimpleChange} from "@angular/core";
+
+import {Exercise} from "../../domain/Exercise/Exercise";
+import {ExerciseBuilder} from "../../domain/Exercise/ExerciseBuilder";
 
 @Component({
     selector: "exercise-form",
     templateUrl: "components/adminArea/ExerciseForm.Component.html"
 })
-export class ExerciseFormComponent {
+export class ExerciseFormComponent implements OnChanges {
+
+    private exerciseBuilder: ExerciseBuilder;
+
+    @Input("exercise")
+    exerciseInput: Exercise;
+
+
+    ngOnChanges(changes: { [propName: string]: SimpleChange }) {
+        let currentValue = changes["exerciseInput"].currentValue;
+        if (currentValue != undefined) {
+            this.exerciseBuilder = ExerciseBuilder.fromExercise(currentValue);
+        } else {
+            this.exerciseBuilder = new ExerciseBuilder();
+        }
+
+    }
 
 }
--- client/app/domain/Exercise/ExerciseBuilder.ts
+++ client/app/domain/Exercise/ExerciseBuilder.ts
@@ -2,13 +2,13 @@ import {Exercise} from "./Exercise";
 
 export class ExerciseBuilder {
 
-    constructor(public id: number, public title: string, public description: string, public lines = new Array<string>()) { }
+    constructor(public id?: number, public title?: string, public description?: string, public lines = new Array<string>()) { }
 
     build(): Exercise {
         return new Exercise(this.id, this.title, this.description, this.lines);
     }
 
-    fromExercise(exercise: Exercise): ExerciseBuilder {
+    static fromExercise(exercise: Exercise): ExerciseBuilder {
         return new ExerciseBuilder(exercise.id, exercise.title, exercise.description, exercise.lines);
     }
 
